04354020e5435009761bee06da7b6a45d0e14125
Iris <irisromeropiquer@gmail.com>
2019-07-24 10:49:26 +0200

    created projectstructure and autoload activated

---
+++ public/index.php
@@ -0,0 +1,6 @@
+<?php 
+
+require_once dirname(__DIR__).'/vendor/autoload.php';
+use App\Kernel;
+
+$kernel= new Kernel();
---
+++ src/Kernel.php
@@ -0,0 +1,8 @@
+<?php
+namespace App;
+
+class Kernel {
+    public function __construct() {
+        echo "hola mundo";
+    }
+}
